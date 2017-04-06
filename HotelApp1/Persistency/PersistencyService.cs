b49de708ddd73625466ddb9973bb70f9db015608
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2017-04-06 12:30:38 +0200

    Tilføjet fields og constructor i 'PersistencyService'

--- HotelApp1/Persistency/PersistencyService.cs
+++ HotelApp1/Persistency/PersistencyService.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace HotelApp1.Persistency
 {
     class PersistencyService
     {
+        const string serverUrl = "http://webservicehotel20170329012418.azurewebsites.net";
+        static HttpClientHandler clientHandler = new HttpClientHandler();
+        static HttpClient client = new HttpClient(clientHandler);
+
+        public PersistencyService()
+        {
+
+        }
+
     }
 }

d66166ae0505371b5d513f4ce57cd70ca06858c4
Lisa Vind Visby Helleberg <lisa4619@edu.easj.dk>
2017-04-06 22:52:05 +0200

    Tilføjet mappen 'Handler' og i den oprettet 'GuestHandler'

--- HotelApp1/ViewModel/GuestViewModel.cs
+++ HotelApp1/ViewModel/GuestViewModel.cs
@@ -4,14 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HotelApp1.Model;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace HotelApp1.ViewModel
 {
-    class GuestViewModel
+    class GuestViewModel : INotifyPropertyChanged
     {
         public int GuestId { get; set; }
         public string Name { get; set; }
         public string Address { get; set; }
+        
+        // ICommands
+        
+        public ObservableCollection<Guest> GuestList { get; set; }
+
+        private Guest selectedGuest;
+
+        public Guest SelectedGuest
+        {
+            get { return selectedGuest; }
+            set
+            {
+                selectedGuest = value;
+                
+            }
+        }
+
+
         public void GemGuest()
         {
             Guest newGuest = new Guest();
